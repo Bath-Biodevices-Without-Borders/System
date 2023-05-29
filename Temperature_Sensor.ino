ac6d51aa2fcd80c03a15f33e0c9f2b48ea2e1f1d
George Madeley <george.madeley@outlook.com>
2023-05-29 15:31:23 +0100

    Preparing for Merge

--- Temperature_Sensor.ino
+++ Temperature_Sensor.ino
@@ -1,67 +1,31 @@
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
-int DS18S20_Pin = 2;  // Signal pin = Digital pin 2.
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 11
 
-// Temperature Chip i/o
-OneWire ds(DS18S20_Pin); // Digital Pin 2.
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
 
-void setup(void) {
-  // put your setup code here, to run once:
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
   Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
 }
 
-void loop(void) {
-  // put your main code here, to run repeatedly:
-  float temperature = getTemp();
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" degrees Celsius.");
-
-  delay(100); // makes Serial Monitor more readable.
-}
-
-
-float getTemp(){
-  // returns the temperature from one DS18S20 in Celsius.
-
-  byte data[12];
-  byte addr[8];
-
-  if( !ds.search(addr)) {
-    // no more sensors on chain, reset search
-    ds.reset_search();
-    return -1000;
-  }
-
-  if( OneWire::crc8( addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid");
-    return -1000;
-  }
-
-  if ( addr[0] != 0x10 && addr[0] != 0x28) {
-    Serial.print("Device is not recognised");
-    return -1000;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44,1); // Start conversion, with parasitic power on at the end.
-
-  byte present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE); // Read Scratchpad.
-  for (int i = 0; i < 9; i++) { // Need 9 bytes.
-    data[i] = ds.read();
-  }
-
-  ds.reset_search();
-
-  byte MSB = data[1];
-  byte LSB = data[0];
-
-  float tempRead = ((MSB << 8) | LSB); // Using two's compliment.
-  float TemperatureSum = tempRead/16;
-
-  return TemperatureSum;
-
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(1000);
 }
